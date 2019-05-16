8aa382367172da74506ac2830850c26977c95fba
Jonathan Hsieh <jonathanhsieh707@gmal.com>
2019-05-16 11:56:37 -0700

    Complete functionality of button to add a new song

--- app/app.js
+++ app/app.js
@@ -16,56 +16,54 @@ var getItem = function(key) {
 }
 
 //create
-var createItem = function(key, value) {
-  return window.localStorage.setItem(key, value);
+var createItem = function() {
+  var songName = $("#songName").val();
+    var artist = $("#artist").val();
+    var bpm = $("#bpm").val();
+    var genre = $("#genre").val();
+    var youtubeLink = $("#youtubeLink").val();
+    var difficulty = $("#difficulty").val();
+
+    var key = artist + ' - ' + songName;
+    var value = {};
+
+    value.songName = songName;
+    value.artist = artist;
+    value.bpm = bpm;
+    value.genre = genre;
+    value.youtubeLink = youtubeLink;
+    value.difficulty = difficulty;
+
+    return window.localStorage.setItem(key, JSON.stringify(value));
 }
 
-//update
-var updateItem = function(key, value) {
-  return window.localStorage.setItem(key, value);
-}
+// //update
+// var updateItem = function(key, value) {
+//   return window.localStorage.setItem(key, value);
+// }
 
-//delete
-var deleteItem = function(key) {
-  return window.localStorage.removeItem(key);
-}
+// //delete
+// var deleteItem = function(key) {
+//   return window.localStorage.removeItem(key);
+// }
 
-//clear everything
-var clearEverything = function() {
-  return window.localStorage.clear();
-}
+// //clear everything
+// var clearEverything = function() {
+//   return window.localStorage.clear();
+// }
 
-var keyExists = function(key) {
-  var currentValue = getItem(key);
-  return currentValue !== null;
-}
+// var keyExists = function(key) {
+//   var currentValue = getItem(key);
+//   return currentValue !== null;
+// }
 
 
 ///////////////////////////////////////////
 //event handlers for the buttons and ... possibly the inputboxes
   //preventdefault on button clicks
 $(document).ready(function() {
-  $('#createButton').click(function(event) {
-    event.preventDefault();
-
-    var currentKey = $("#keyInput").val();
-    var currentValue = $("#valueInput").val();
-    if (keyExists(currentKey)) {
-      //current key exists, do something error-handle-y
-    } else {
-      createItem(currentKey, currentValue);
-    }
-  });
-
-  $('#updateButton').click(function(event) {
-    event.preventDefault();
-
-    var currentKey = $("#keyInput").val();
-    var currentValue = $("#valueInput").val();
-    if (keyExists(currentKey)) {
-      updateItem(currentKey, currentValue);
-    } else {
-      //current key doesnt exist, do stuff
-    }
+  $('#addSong').click(function(event) {
+    // event.preventDefault();
+    createItem();
   });
 });
